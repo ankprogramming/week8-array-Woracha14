081159ea05e69122b123cfb8a4120f2bb2e6885c
Unknown <milk756@hotmail.com>
2018-01-28 22:52:49 +0700

    ทำข้อที่ 2

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -11,6 +11,18 @@ int main () {
         // TODO: เขียนโปรแกรมเพื่อสลับชื่อ
         // hint : สร้าง array มารับค่าตัวอักษรแบบย้อนกลับ สามารถใช้ loop เข้ามาช่วยได้
 
+        char revName[nameLength+1];
+        for (int i=0; i<nameLength; i++){
+            revName [i] = name[5-i];
+        }
+
+        revName[nameLength] = '\0';
+        cout << revName << endl;
+
+        for (int i=0; i<6; i++) {
+            revName [i] =revName [i] + 1;
+        }
+        cout << revName ;
 
 
         return 0;
